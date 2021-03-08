e900e61d0463f955f679c4e4790bd0b4fc9d10fb
akifkartal03 <akokartal@gmail.com>
2021-03-08 14:49:57 +0300

    start to new project

---
+++ Start/header.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<time.h>
+#include <sys/types.h> 
+#include<string.h>
+#include<math.h>
+
+void createTree(int height);
+void manageLeafNode();
+void manageParent(int pid1, int pid2);
+#endif
--- Start/main.c
+++ Start/main.c
@@ -1,10 +1,69 @@
-#include<stdio.h>
-#include<stdlib.h>
 
+
+#include "header.h"
 int main(){
-    int *x = malloc(sizeof(int)*10);
-    x[5] = 10;
-    printf("leaks try!!\n");
-    free(x);
+    
     return 0;
+}
+
+void createTree(int height){
+    if (height == 0)
+    {
+        manageLeafNode();
+        return;
+    }
+    int pid = fork();
+    if (pid > 0)
+    {
+        //parent process
+        int pid2 = fork();
+        if (pid2 > 0)
+        {
+            //parent process
+            manageParent(pid,pid2);
+
+        }
+        else if (pid2 == 0)
+        {
+            //left child
+            createTree(--height);
+        }
+        
+    }
+    else if (pid == 0)
+    {
+        //right child
+        createTree(--height);
+    }
+    
+    
+    
+}
+void manageLeafNode(){
+    char filename[20];
+    //make file name process id
+    sprintf(filename,"%d.txt",getpid());
+    //open file by using system call
+    int fd = open(filename,O_RDONLY | O_CREAT);
+    if (fd == -1){
+        //denote system call error
+        fprintf(stderr,"open file error!!\n");
+        exit(EXIT_FAILURE);
+    }
+    //create random number
+    srand(getpid());
+    int rnd = rand()/100;
+    size_t size = floor (log10 (abs (rnd))) + 1;
+    int res = write(fd,rnd,size);
+    if (res == -1)
+    {
+        fprintf(stderr,"write to file error");
+        exit(EXIT_FAILURE);
+    }
+    if (close(fd) < 0)
+    {
+        fprintf(stderr,"write to file error");
+        exit(EXIT_FAILURE);
+    }
+
 }
