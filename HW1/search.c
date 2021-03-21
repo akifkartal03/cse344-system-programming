5ea024f2dbdc191e2d368792a0e02b439c15fbbc
akifkartal03 <akokartal@gmail.com>
2021-03-21 19:45:04 +0300

    refactoring

---
+++ HW1/search.c
@@ -0,0 +1,55 @@
+#include "search.h"
+
+void traversePathRecursively(char *targetPath){
+    char currentPath[PATH_MAX];
+    DIR *dir;
+    struct dirent *entry;
+    size_t pathLength = strlen(targetPath);
+    if (pathLength >= PATH_MAX)
+    {
+        fprintf(stderr, "Path length is longer than expected!\n");
+        exit(EXIT_FAILURE);
+    }
+    dir = opendir(targetPath);
+    if (!dir)
+    {
+       return;
+    }
+    while ((entry = readdir(dir)) != NULL)
+    {
+       //check directory is different than current and parent
+       if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+        {
+            printf("%s\n", entry->d_name);
+
+            strcpy(currentPath, targetPath);
+            strcat(currentPath, "/");
+            strcat(currentPath, entry->d_name);
+            size_t pathLength = strlen(currentPath);
+            if (pathLength >= PATH_MAX)
+            {
+                fprintf(stderr, "Path length is longer than expected!\n");
+                exit(EXIT_FAILURE);
+            }
+            traversePathRecursively(currentPath);
+        }
+    }
+    if (close(dir))
+    {
+        fprintf(stderr, "Directory close error! %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+}
+void checkGivenArguments(const char *path,const args givenArgs);
+void showSearchResults(int isFound);
+void drawTree(char *targetPath , char *fileName);
+
+/*int main(int argc, char *argv[])
+{
+    //args a;
+    traversePathRecursively(argv[1]);
+    //printf("count: %d\n",a.count);
+    //printf("filename main: %s\n",a.fArg);
+    return 0;
+}*/
