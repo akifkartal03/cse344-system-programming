7b9b8bac1119f61f0d8067ce6f8a3dbc1506e846
akifkartal03 <akokartal@gmail.com>
2021-04-08 01:08:58 +0300

    last but not least

--- HW2/helper.c
+++ HW2/helper.c
@@ -1,4 +1,39 @@
 #include "helper.h"
+static volatile int numLiveChildren = 0;
+pid_t arr[8];
+void errExit(char *msg)
+{
+    printf("%s error!\n", msg);
+    exit(EXIT_FAILURE);
+}
+void handler(int sig)
+{
+    //printf("sigHander: %d\n",counter);
+    //counter--;
+}
+void handler2(int sig)
+{
+    //printf("sigHander: %d\n",counter);
+    //counter--;
+}
+void sigchldHandler(int sig)
+{
+    int status, savedErrno;
+    pid_t childPid;
+
+    savedErrno = errno;
+
+    /* Do nonblocking waits until no more dead children are found */
+
+    while ((childPid = waitpid(-1, &status, WNOHANG)) > 0)
+    {
+        numLiveChildren--;
+    }
+
+    if (childPid == -1 && errno != ECHILD)
+        errExit("waitpid");
+    errno = savedErrno;
+}
 
 void checkArgument(int argc)
 {
@@ -72,27 +107,28 @@ int safeLseek(int fd, int offset, int whence)
 }
 void lockFile(int fd, struct flock *fl, int read);
 void unlockFile(int fd, struct flock *fl);
-void writeEndofLine(int fd,double number,int line){
+void writeEndofLine(int fd, double number, int line)
+{
     char c;
-    safeLseek(fd,1,SEEK_SET);
+    safeLseek(fd, 1, SEEK_SET);
     for (int i = 0; i < line; i++)
     {
-        safeRead(fd,&c,1);
-        while (c !='\n')
+        safeRead(fd, &c, 1);
+        while (c != '\n')
         {
-            safeLseek(fd,1,SEEK_CUR);
-            safeRead(fd,&c,1);
+            safeLseek(fd, 1, SEEK_CUR);
+            safeRead(fd, &c, 1);
         }
     }
-    safeLseek(fd,-1,SEEK_CUR);
+    safeLseek(fd, -1, SEEK_CUR);
     char buffer[30];
-    int n = sprintf(buffer, ",%.1f,%.1f\n",number,number);
-    printf("n:%d\n%s\n",n,buffer);
-    int a = safeWrite(fd,buffer,n);
-    printf("a:%d\n",a);
+    int n = sprintf(buffer, ",%.1f,%.1f\n", number, number);
+    printf("n:%d\n%s\n", n, buffer);
+    int a = safeWrite(fd, buffer, n);
+    printf("a:%d\n", a);
 }
 
-double calculateInterpolation(double x[], double y[], int xCount,int count)
+double calculateInterpolation(double x[], double y[], int xCount, int count)
 {
     double yResult = 0;
     for (int i = 0; i < count; i++)
@@ -107,29 +143,31 @@ double calculateInterpolation(double x[], double y[], int xCount,int count)
     }
     return yResult;
 }
-void testLagrange(char *buff,int count){
+void testLagrange(char *buff, int count)
+{
     char *pt;
-    pt = strtok (buff,",");
+    pt = strtok(buff, ",");
     int counter = 3;
-    double x[count],y[count],xi,yi;
+    double x[count], y[count], xi, yi;
     int i = 0;
     double temp;
     const int cplus1 = count + 1;
     const int citself = count;
-    while (pt != NULL) {
+    while (pt != NULL)
+    {
         double a = atof(pt);
-        if (i>count+1)
+        if (i > count + 1)
         {
             break;
         }
         if (i == count)
         {
-            xi=a;
+            xi = a;
             i++;
         }
         if (i == count + 1)
         {
-            yi=a;
+            yi = a;
             i++;
         }
         if (counter % 2 == 0 && i < citself)
@@ -138,11 +176,11 @@ void testLagrange(char *buff,int count){
             y[i] = a;
             i++;
         }
-        pt = strtok (NULL, ",");
+        pt = strtok(NULL, ",");
         temp = a;
         counter++;
     }
-    printf("res: %.1f\n",calculateInterpolation(x,y,xi,4));
+    printf("res: %.1f\n", calculateInterpolation(x, y, xi, 4));
 }
 int main(int argc, char *argv[])
 {
@@ -154,7 +192,7 @@ int main(int argc, char *argv[])
     int i = 0;
     char c;
     int fd = safeOpen(path, O_RDONLY);
-    char *buffer = (char *) calloc(100, sizeof(char));
+    char *buffer = (char *)calloc(100, sizeof(char));
     /* Read from the file, one chunk at a time. Continue until read “comes up short”, that is, 
     reads less than we asked for. This indicates that we’ve hit the end of the file.
     do
@@ -206,9 +244,130 @@ int main(int argc, char *argv[])
     //writeEndofLine(fd2,8.0,1);
 
     //free(buffer);
+    pid_t pid;
+    int j, sigCnt;
+    sigset_t blockMask, emptyMask, blockMask2, emptyMask2;
+    struct sigaction sa, sa2;
 
-    
+    setbuf(stdout, NULL); /* Disable buffering of stdout */
 
+    sigCnt = 0;
+    numLiveChildren = 8;
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = &sigchldHandler;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1)
+        errExit("sigaction");
+
+    sigemptyset(&sa2.sa_mask);
+    sa2.sa_flags = 0;
+    sa2.sa_handler = &handler;
+    if (sigaction(SIGUSR1, &sa2, NULL) == -1)
+        errExit("sigaction");
+    /* Block SIGCHLD to prevent its delivery if a child terminates
+       before the parent commences the sigsuspend() loop below */
+
+    sigemptyset(&blockMask);
+    sigaddset(&blockMask, SIGCHLD);
+    if (sigprocmask(SIG_SETMASK, &blockMask, NULL) == -1)
+        errExit("sigprocmask");
+
+    sigemptyset(&blockMask2);
+    sigaddset(&blockMask2, SIGUSR1);
+    if (sigprocmask(SIG_SETMASK, &blockMask2, NULL) == -1)
+        errExit("sigprocmask");
+
+    /* Create one child process for each command-line argument */
+
+    for (j = 1; j < 9; j++)
+    {
+        pid = fork();
+        if (pid == -1)
+        {
+            errExit("fork");
+        }
+        else if (pid == 0)
+        {
+            /* Child Processs */
+
+            /*Wait for sigusr1 signal*/
+            struct sigaction sa3;
+            sigemptyset(&sa3.sa_mask);
+            sa3.sa_flags = 0;
+            sa3.sa_handler = &handler2;
+            if (sigaction(SIGUSR1, &sa3, NULL) == -1)
+                errExit("sigaction");
+            /*wait first signal*/
+            sigset_t emptyMask3;
+            sigemptyset(&emptyMask3);
+            if (sigsuspend(&emptyMask3) == -1 && errno != EINTR)
+                errExit("sigsuspend");
+
+            do
+            {
+
+                bytes_read = safeRead(fd, &c, 1);
+                offset += bytes_read;
+                if (capacity <= offset + 1)
+                {
+                    capacity = capacity + 100;
+                    buffer = realloc(buffer, capacity * sizeof(char));
+                }
+                buffer[i] = c;
+                i++;
+
+            } while (bytes_read == 1);
+            testLagrange(buffer,4);
+            //close(fd);
+            free(buffer);
+            printf("Child %d (PID=%ld) signaling to parent...\n", j, (long)getpid());
+            /*Signal to the parent*/
+            if (j == 8)
+            {
+                kill(getppid(), SIGUSR1);
+            }
+            /*wait second signal*/
+            sigemptyset(&emptyMask3);
+            if (sigsuspend(&emptyMask3) == -1 && errno != EINTR)
+                errExit("sigsuspend");
+
+            printf("Child (PID=%ld) GOT the signal...\n", (long)getpid());
+            _exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            /*Parent Process*/
+            arr[j - 1] = pid;
+            printf("signal to the... (PID=%ld)\n", (long)pid);
+            kill(pid,SIGUSR1);
+            //printf("j: %d\n",j);
+            //counter--;
+        }
+    }
+    /*END OF FOR LOOP*/
+    sigemptyset(&emptyMask2);
+    if (sigsuspend(&emptyMask2) == -1 && errno != EINTR)
+        errExit("sigsuspend");
+    printf("Parent[%ld] has finished its waitng for round1..\n", (long)getpid());
+    //printf("counter: %d\n",counter);
+    /*Calculate firs errrorr!!!*/
+    for (int i = 0; i < 8; i++)
+    {
+        kill(arr[i], SIGUSR1);
+    }
+    /*SECOUNd*/
+    sigemptyset(&emptyMask);
+    while (numLiveChildren > 0)
+    {
+        if (sigsuspend(&emptyMask) == -1 && errno != EINTR)
+            errExit("sigsuspend");
+        sigCnt++;
+    }
+    printf("Parent (PID=%ld) exiting 2.error calculation\n", (long)getpid());
+    printf("All children have terminated; SIGCHLD was caught "
+           "%d times\n",
+           sigCnt);
 
     return 0;
 }
--- Start/signal/mymask6.c
+++ Start/signal/mymask6.c
@@ -151,5 +151,5 @@ int main(int argc, char *argv[])
            "%d times\n",
            sigCnt);
 
-    exit(EXIT_SUCCESS);
+    return 0;
 }
