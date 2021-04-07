572cd80af0b1d03838800528c89cb5f382cf3c9f
akifkartal03 <akokartal@gmail.com>
2021-04-07 10:09:48 +0300

    add work

---
+++ Start/signal/mymask6.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <string.h>
+#include <math.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+static volatile int numLiveChildren = 0;
+static volatile int counter = 0;
+pid_t arr[8];
+void errExit(char *msg)
+{
+    printf("%s error!\n", msg);
+    exit(EXIT_FAILURE);
+}
+void handler(int sig)
+{
+    printf("sigHander: %d\n",counter);
+    counter--;
+}
+void handler2(int sig)
+{
+    //printf("sigHander: %d\n",counter);
+    //counter--;
+}
+void sigchldHandler(int sig)
+{
+    int status, savedErrno;
+    pid_t childPid;
+
+    savedErrno = errno;
+
+ 
+    /* Do nonblocking waits until no more dead children are found */
+
+    while ((childPid = waitpid(-1, &status, WNOHANG)) > 0)
+    {
+        numLiveChildren--;
+    }
+
+    if (childPid == -1 && errno != ECHILD)
+        errExit("waitpid");
+    errno = savedErrno;
+}
+int main(int argc, char *argv[])
+{
+    pid_t pid;
+    int j, sigCnt;
+    sigset_t blockMask, emptyMask,blockMask2, emptyMask2;
+    struct sigaction sa,sa2;
+
+    setbuf(stdout, NULL); /* Disable buffering of stdout */
+
+    sigCnt = 0;
+    numLiveChildren = 8;
+    counter = 8;
+    int r = 1;
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = &sigchldHandler;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1)
+        errExit("sigaction");
+
+    sigemptyset(&sa2.sa_mask);
+    sa2.sa_flags = 0;
+    sa2.sa_handler = &handler;
+    if (sigaction(SIGUSR1, &sa2, NULL) == -1)
+        errExit("sigaction");
+    /* Block SIGCHLD to prevent its delivery if a child terminates
+       before the parent commences the sigsuspend() loop below */
+
+    sigemptyset(&blockMask);
+    sigaddset(&blockMask, SIGCHLD);
+    if (sigprocmask(SIG_SETMASK, &blockMask, NULL) == -1)
+        errExit("sigprocmask");
+    
+    sigemptyset(&blockMask2);
+    sigaddset(&blockMask2, SIGUSR1);
+    if (sigprocmask(SIG_SETMASK, &blockMask2, NULL) == -1)
+        errExit("sigprocmask");
+
+    /* Create one child process for each command-line argument */
+
+    for (j = 1; j < 9; j++)
+    {
+        switch (pid = fork())
+        {
+        case -1:
+            errExit("fork");
+
+        case 0: /* Child - sleeps and then exits */
+            signal(SIGUSR1, handler2);
+            printf("Child %d (PID=%ld) exiting...\n", j, (long)getpid());
+            if (r == 1)
+            {
+                r = 2;
+                printf("kill: %ld\n",(long)getpid());
+                kill(getppid(), SIGUSR1);
+                break;
+            }
+            else{
+                printf("exit: %ld\n",(long)getpid());
+                _exit(EXIT_SUCCESS);
+            }
+            
+        default: /* Parent - loops to create next child */
+            arr[j-1] = pid;
+            break;
+        }
+    }
+    sigemptyset(&emptyMask2);
+    while (counter > 0) //sleep
+    {
+        if (sigsuspend(&emptyMask2) == -1 && errno != EINTR)
+            errExit("sigsuspend");
+        sigCnt++;
+    }
+    printf("Parent[%ld] has finished its waitng for round1..\n",(long)getpid());
+    printf("counter: %d\n",counter);
+    /*Calculate firs errrorr!!!*/
+    for (int i = 0; i < 8; i++)
+    {
+        kill(arr[i], SIGUSR1);
+    }
+    
+    sigemptyset(&emptyMask);
+    while (numLiveChildren > 0)
+    {
+        if (sigsuspend(&emptyMask) == -1 && errno != EINTR)
+            errExit("sigsuspend");
+        sigCnt++;
+    }
+    printf("Parent (PID=%ld) exiting 2.error calculation\n", (long)getpid());
+    printf("All children have terminated; SIGCHLD was caught "
+           "%d times\n",
+           sigCnt);
+
+    exit(EXIT_SUCCESS);
+}
