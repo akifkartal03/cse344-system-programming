b2e09a82cb5ae147a66a278905f40743247491d1
akifkartal03 <akokartal@gmail.com>
2021-04-21 03:20:47 +0300

    add last

--- HW3/execute.sh
+++ HW3/execute.sh
@@ -1,3 +1,2 @@
 gnome-terminal -- ./player -b 0 -s mymemory45 -f /home/akif/Desktop/source/example -m sem45 &
 gnome-terminal -- ./player -b 2 -s mymemory45 -f /home/akif/Desktop/source/example -m sem45 &
-gnome-terminal -- ./player -b 3 -s mymemory45 -f /home/akif/Desktop/source/example -m sem45 &
--- HW3/peer.c
+++ HW3/peer.c
@@ -7,6 +7,8 @@ static void removeAll(void)
 {
     unlink(myFifoName);
     sem_unlink(semphoreName);
+    sem_unlink("counter");
+    sem_unlink("barrier");
     shm_unlink(memoryName);
 }
 void exitHandler(int signal)
@@ -24,24 +26,34 @@ int main(int argc, char *argv[])
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = &exitHandler;
     sigaction(SIGINT, &sa, NULL);
-    
 
     struct args givenParams;
     checkArguments(argc, argv, &givenParams);
-    
-    //printf("contuine!!!\n");
+
     int myFd, dummyFd, reciverFd, fd, fifoNames;
     struct stat sb;
     player *data;
     mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH | S_IRWXU;
     sem_t *sem_id = sem_open(givenParams.mArg, O_CREAT, 0666, 1);
+
     if (sem_id == SEM_FAILED)
+        errExit("sem_open error!");
+    fifoNames = safeOpen(givenParams.fArg, O_RDWR);
+    int n = getNumberOfLine(fifoNames);
+    sem_t *sem_count = sem_open("counter", O_CREAT, 0666, n + 1);
+
+    if (sem_count == SEM_FAILED)
+        errExit("sem_open error!");
+    sem_t *sem_barrier = sem_open("barrier", O_CREAT, 0666, 0);
+
+    if (sem_barrier == SEM_FAILED)
         errExit("sem_open error!");
 
     if (sem_wait(sem_id) == -1)
         errExit("sem_wait");
-    
-    fifoNames = safeOpen(givenParams.fArg, O_RDWR);
+    if (sem_wait(sem_count) == -1)
+        errExit("sem_wait");
+
     fd = shm_open(givenParams.sArg, O_CREAT | O_RDWR, mode);
     if (fd == -1)
         errExit("shm_open error!");
@@ -56,18 +68,16 @@ int main(int argc, char *argv[])
     data = (player *)mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (data == MAP_FAILED)
         errExit("mmap");
-    int n = getNumberOfLine(fifoNames);
     int res = 0, k = 1;
     char *name;
     do
     {
         name = readLine(fifoNames, k);
-        //printf("name:%s\n",name);
+
         for (int i = 0; i < n; ++i)
         {
-            //printf("data: %s\n",data[i].fifo_name);
+
             res = strcmp(name, data[i].fifo_name);
-            //printf("res:%d\n",res);
             if (res == 0)
             {
                 break;
@@ -76,15 +86,12 @@ int main(int argc, char *argv[])
         k++;
     } while (res == 0);
     k--;
-    strcpy(memoryName,givenParams.sArg);
-    strcpy(myFifoName,name);
-    strcpy(semphoreName,givenParams.mArg);
-    if(atexit(removeAll) != 0)
+    strcpy(memoryName, givenParams.sArg);
+    strcpy(myFifoName, name);
+    strcpy(semphoreName, givenParams.mArg);
+    if (atexit(removeAll) != 0)
         errExit("atexit");
-    
-    printf("name:%s\n",name);
-    printf("k:%d\n",k);
-    printf("run_pid=%ld\n",(long)getpid());
+
     int numberOfSwitch = atoi(givenParams.bArg);
     player info;
     strcpy(info.fifo_name, name);
@@ -92,26 +99,47 @@ int main(int argc, char *argv[])
     info.pot_pid = getpid();
     info.done = 1;
     memcpy(&data[k - 1], &info, sizeof(info));
+    int counter;
+    if (sem_getvalue(sem_count, &counter) == -1)
+        errExit("sem_get");
+
+    if (counter <= 1)
+    {
+        if (sem_post(sem_barrier) == -1)
+            errExit("sem_post");
+    }
     if (sem_post(sem_id) == -1)
         errExit("sem_post");
-    
+
+    if (sem_wait(sem_barrier) == -1)
+        errExit("sem_wait");
+
+    if (sem_post(sem_barrier) == -1)
+        errExit("sem_post");
     umask(0);
     if (numberOfSwitch > 0)
     {
-        
-        int rnd;
-        do
+        char randFifoName[50];
+        int is_found = 0;
+        for (int i = 0; i < n; i++)
         {
-            rnd = getRandom(getNumberOfLine(fifoNames));
-        } while (rnd == k);
-        printf("ICERDE LOOPDAN SONRA!!!!\n");
-        char *randFifoName = readLine(fifoNames, rnd);
+            if (data[i].switches == 0)
+            {
+                strcpy(randFifoName, data[i].fifo_name);
+                is_found = 1;
+                break;
+            }
+        }
+        if (!is_found)
+        {
+            printf("There should be at least 1 process with 0 potato!\n");
+            exit(EXIT_FAILURE);
+        }
         if (mkfifo(randFifoName, mode) == -1 && errno != EEXIST)
             errExit("mkfifo error!");
         reciverFd = open(randFifoName, O_WRONLY);
         if (reciverFd == -1)
             errExit("open fifo error!");
-        //printf("randfile:%s\n", randFifoName);
         int recFd = open(randFifoName, O_RDONLY);
         if (recFd == -1)
             errExit("open fifo error!");
@@ -121,24 +149,17 @@ int main(int argc, char *argv[])
                (long)getpid(), (long)getpid(), randFifoName);
         struct sender req;
         req.pid = getpid();
-        strcpy(req.fifo_name,name);
-        printf("ICERDE LOOPDAN SONRA!!!!\n");
+        strcpy(req.fifo_name, name);
         if (write(reciverFd, &req, sizeof(struct sender)) != sizeof(struct sender))
             errExit("Can't send potato!");
-        free(randFifoName);
-        printf("ICERDE WRITEEE SONRA!!!!\n");
         if (close(reciverFd))
             errExit("close error!!");
         if (close(recFd))
             errExit("close error!!");
-
-        
-        
     }
-    printf("DISARDAAAAA!!!\n");
     if (mkfifo(name, mode) == -1 && errno != EEXIST)
         errExit("mkfifo error!");
-    myFd = open(name, O_RDONLY); // kendi fifosunu açıyor
+    myFd = open(name, O_RDONLY);
     if (myFd == -1)
         errExit("open fifo error!");
     dummyFd = open(name, O_WRONLY);
@@ -158,7 +179,6 @@ int main(int argc, char *argv[])
             free(name);
             exit(EXIT_SUCCESS);
         }
-        printf("wait önuu\n");
         if (sem_wait(sem_id) == -1)
             errExit("sem_wait");
         int index;
@@ -166,17 +186,14 @@ int main(int argc, char *argv[])
         {
             if (data[i].pot_pid == resp.pid)
             {
-               index = i;
+                index = i;
             }
-            
         }
         printf("pid=%ld receiving potato number %ld from %s\n", (long)getpid(), (long)resp.pid, resp.fifo_name);
-        //player updatedInfo;
-        
+
         data[index].switches = data[index].switches - 1;
         data[index].done = data[index].done + 1;
-        printf("switch_pid: %ld\n",(long)data[index].pot_pid);
-        printf("switch_number: %d\n",data[index].switches);
+
         if (sem_post(sem_id) == -1)
             errExit("sem_post");
         if (data[index].switches > 0)
@@ -196,10 +213,10 @@ int main(int argc, char *argv[])
             if (recFd == -1)
                 errExit("open fifo error!");
             printf("pid=%ld sending potato number %ld to %s; this is switch number %d\n",
-                (long)getpid(), (long)resp.pid, randFifoName, data[index].done);
+                   (long)getpid(), (long)resp.pid, randFifoName, data[index].done);
             struct sender req;
-            req.pid =resp.pid;
-            strcpy(req.fifo_name,name);
+            req.pid = resp.pid;
+            strcpy(req.fifo_name, name);
             if (write(reciverFd, &req, sizeof(struct sender)) != sizeof(struct sender))
                 errExit("Can't send potato!");
             free(randFifoName);
@@ -208,8 +225,9 @@ int main(int argc, char *argv[])
             if (close(recFd))
                 errExit("close error!!");
         }
-        else{
-            printf("pid=%ld potato number %ld has cooled down\n",(long)getpid(), (long)resp.pid);
+        else
+        {
+            printf("pid=%ld potato number %ld has cooled down\n", (long)getpid(), (long)resp.pid);
             //check last potato
             int last = 1;
             for (int i = 0; i < n; ++i)
@@ -222,8 +240,7 @@ int main(int argc, char *argv[])
             }
             if (last)
             {
-                
-                printf("this is last!!!\n");
+
                 for (int i = 1; i <= n; i++)
                 {
                     if (i != k)
@@ -250,8 +267,6 @@ int main(int argc, char *argv[])
                 break;
             }
         }
-      
-        
     }
 
     return 0;
