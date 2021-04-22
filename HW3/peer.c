8db15b37afb2de56014a222c7e9e3a5093dd42e4
akifkartal03 <akokartal@gmail.com>
2021-04-22 05:33:55 +0300

    last

--- HW3/peer.c
+++ HW3/peer.c
@@ -1,8 +1,11 @@
 #include "helper.h"
 
+/*global names to remove*/
 static char myFifoName[50];
 static char memoryName[50];
 static char semphoreName[50];
+
+/*remove function before exit*/
 static void removeAll(void)
 {
     unlink(myFifoName);
@@ -20,42 +23,52 @@ int main(int argc, char *argv[])
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = &exitHandler;
     sigaction(SIGINT, &sa, NULL);
+
+    /*Check arguments*/
     srand(time(0));
     struct args givenParams;
     checkArguments(argc, argv, &givenParams);
 
+    /*create important variables*/
     int myFd, dummyFd, reciverFd, fd, fifoNames,tempFd;
     struct stat sb;
     player *data;
     mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH | S_IRWXU;
-    sem_t *sem_id = sem_open(givenParams.mArg, O_CREAT, 0666, 1);
 
-    if (sem_id == SEM_FAILED)
-        errExit("sem_open error!");
+    /*open fifo names file and get number of lines*/
     fifoNames = safeOpen(givenParams.fArg, O_RDWR);
     int n = getNumberOfLine(fifoNames);
-    sem_t *sem_count = sem_open("counter", O_CREAT, 0666, n + 1);
 
+    /*open given semaphore*/
+    sem_t *sem_id = sem_open(givenParams.mArg, O_CREAT, 0666, 1);
+    if (sem_id == SEM_FAILED)
+        errExit("sem_open error!");
+
+    /*define other semaphores to make synchronization between process*/
+    sem_t *sem_count = sem_open("counter", O_CREAT, 0666, n + 1);
     if (sem_count == SEM_FAILED)
         errExit("sem_open error!");
     sem_t *sem_barrier = sem_open("barrier", O_CREAT, 0666, 0);
-
     if (sem_barrier == SEM_FAILED)
         errExit("sem_open error!");
-    
     sem_t *sem_fifo_barrier = sem_open("fifo_barrier", O_CREAT, 0666, 0);
-
     if (sem_fifo_barrier == SEM_FAILED)
         errExit("sem_open error!");
 
+    /*enter critical region with given semaphore*/
     if (sem_wait(sem_id) == -1)
         errExit("sem_wait");
+
+    /*decrement counter semaphore*/    
     if (sem_wait(sem_count) == -1)
         errExit("sem_wait");
 
+    /*open shared memory*/
     fd = shm_open(givenParams.sArg, O_CREAT | O_RDWR, mode);
     if (fd == -1)
         errExit("shm_open error!");
+
+    /*adjust it's size if needed*/    
     if (fstat(fd, &sb) == -1)
         errExit("fstat error");
     size_t len = 4096;
@@ -69,9 +82,13 @@ int main(int argc, char *argv[])
         printf("exiting....\n");
         exit(EXIT_FAILURE);
     }
+
+    /*attach itself to the shared memory segment*/
     data = (player *)mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (data == MAP_FAILED)
         errExit("mmap");
+
+    /*find unique fifo name using shared memory*/    
     int res = 0, k = 1;
     char *name;
     do
@@ -94,11 +111,17 @@ int main(int argc, char *argv[])
         }
     } while (res == 0);
     k--;
+
+    /*after finding name adjust variables*/
     strcpy(memoryName, givenParams.sArg);
     strcpy(myFifoName, name);
     strcpy(semphoreName, givenParams.mArg);
+
+    /*save remove method*/
     if (atexit(removeAll) != 0)
         errExit("atexit");
+
+    /*get real name of fifo(aliveli etc.)*/    
     char *ch;
     char temp_name[50];
     char realt_name[30];
@@ -108,6 +131,8 @@ int main(int argc, char *argv[])
         strcpy(realt_name,ch);
         ch = strtok(NULL, "/");
     }
+
+    /*add potato to the shared memory*/
     int numberOfSwitch = atoi(givenParams.bArg);
     player info;
     strcpy(info.fifo_name, name);
@@ -117,12 +142,22 @@ int main(int argc, char *argv[])
     info.done = 1;
     info.is_opened = 0;
     memcpy(&data[k - 1], &info, sizeof(info));
+
+    /*create fifo*/
     umask(0);
     if (mkfifo(name, mode) == -1 && errno != EEXIST)
         errExit("mkfifo error!");
+
+    /*get counter semaphore value*/    
     int counter;
     if (sem_getvalue(sem_count, &counter) == -1)
         errExit("sem_get");
+
+    /*
+    * if this is not first running process,
+    * check is there a waiting fifo to open
+    * and open it in write mode.
+    */
     if (counter != n)
     {
         for (int i = 0; i < n; i++)
@@ -142,13 +177,18 @@ int main(int argc, char *argv[])
             }
         }  
     }
+    /*if this is last process running, free the semophore
+    * so that other process can contiune
+    */
     if (counter <= 1)
     {
         if (sem_post(sem_fifo_barrier) == -1)
             errExit("sem_post");
     }
+    /*free given critical region semaphore*/
     if (sem_post(sem_id) == -1)
         errExit("sem_post");
+    /*open your fifo both reading and wiriting mode*/
     myFd = open(name, O_RDONLY);
     if (myFd == -1)
         errExit("open fifo error!");
@@ -158,10 +198,20 @@ int main(int argc, char *argv[])
     if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
         errExit("signal");
 
+    /*if you can open your fifo wait for last process
+    * free the semaphore so you can contiune.
+    */    
      if (sem_wait(sem_fifo_barrier) == -1)
         errExit("sem_wait");
+
+    /*aftercontiune, post again for other process*/
     if (sem_post(sem_fifo_barrier) == -1)
         errExit("sem_post");
+
+    /*
+    * and then lastly check is there any waiting process 
+    * to open it's fifo, if any open it
+    */    
     for (int i = 0; i < n; i++)
     {
         if ((data[i].is_opened == 0) && (strlen(data[i].fifo_name) > 0) &&  (data[i].pot_pid != getpid())
@@ -185,22 +235,36 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     
-
+    /*
+    * check all process have finished
+    *their opening and initilaziton
+    */
     if (counter <= 1)
     {
         if (sem_post(sem_barrier) == -1)
             errExit("sem_post");
     }
     
-
+    /*
+    * if there is a process still is not finished
+    * it's opening and initilaziton then wait.
+    */
     if (sem_wait(sem_barrier) == -1)
         errExit("sem_wait");
 
+    /*
+    * if all process have finished it's opening 
+    * and initilaziton first post to others and contiune.
+    */
     if (sem_post(sem_barrier) == -1)
         errExit("sem_post");
+
+    printf("\n");
     if (numberOfSwitch > 0)
     {
-      
+        /*
+        * if the process has potato send it another one 
+        */
         int rnd;
         do
         {
@@ -214,27 +278,7 @@ int main(int argc, char *argv[])
             strcpy(randRealName,ch);
             ch = strtok(NULL, "/");
         }
-        /*
-        for (int i = 0; i < n; i++)
-        {
-            if (data[i].switches == 0)
-            {
-                strcpy(randFifoName, data[i].fifo_name);
-                strcpy(randRealName, data[i].real_name);
-                is_found = 1;
-                break;
-            }
-            if (exitSignal)
-            {
-                printf("exiting....\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-        if (!is_found)
-        {
-            printf("There should be at least 1 process with 0 potato!\n");
-            exit(EXIT_FAILURE);
-        }*/
+        
         if (exitSignal)
         {
             printf("exiting....\n");
@@ -254,7 +298,10 @@ int main(int argc, char *argv[])
             errExit("close error!!");
         free(randFifoName);
     }
-    
+     /*
+    * if the process has not a potato or already send it
+    * then wait until one of them come. 
+    */
     while (1)
     {
         if (exitSignal)
@@ -263,15 +310,18 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
         struct sender resp;
+        /*wait here by reading fifo*/
         if (read(myFd, &resp, sizeof(struct sender)) != sizeof(struct sender))
         {
             continue;
         }
+        /*exit message*/
         if (resp.pid == -1)
         {
             free(name);
             exit(EXIT_SUCCESS);
         }
+         /*critical region update shared memory*/
         if (sem_wait(sem_id) == -1)
             errExit("sem_wait");
         int index;
@@ -294,8 +344,10 @@ int main(int argc, char *argv[])
 
         if (sem_post(sem_id) == -1)
             errExit("sem_post");
+        /*exit critical region and switch number*/    
         if (data[index].switches > 0)
         {
+            /*send it again*/  
             int rnd;
             do
             {
@@ -349,7 +401,7 @@ int main(int argc, char *argv[])
             }
             if (last)
             {
-
+                /*it was last so send exit message all other process*/  
                 for (int i = 1; i <= n; i++)
                 {
                     if (i != k)
@@ -372,6 +424,7 @@ int main(int argc, char *argv[])
                         exit(EXIT_FAILURE);
                     }
                 }
+                /*exit itself*/  
                 free(name);
                 break;
             }
