f7070f130e79eaf36fd9dc08d9f1b93fed75c3ad
akifkartal03 <akokartal@gmail.com>
2021-06-08 15:23:02 +0300

    last

--- Final Project/run.sh
+++ Final Project/run.sh
@@ -1,3 +1,7 @@
 gnome-terminal -- ./client -i 1 -a 127.0.0.1 -p 3456 -o example &
 gnome-terminal -- ./client -i 2 -a 127.0.0.1 -p 3456 -o example &
 gnome-terminal -- ./client -i 3 -a 127.0.0.1 -p 3456 -o example &
+gnome-terminal -- ./client -i 4 -a 127.0.0.1 -p 3456 -o example &
+gnome-terminal -- ./client -i 5 -a 127.0.0.1 -p 3456 -o example &
+gnome-terminal -- ./client -i 6 -a 127.0.0.1 -p 3456 -o example &
+gnome-terminal -- ./client -i 7 -a 127.0.0.1 -p 3456 -o example &
